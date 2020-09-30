fdbcc613a35e8c749aa3bf629d7d44339edb6757
Charindya <charindya@gmail.com>
2020-09-30 13:39:24 +1000

    intial commit

---
+++ build/precache-manifest.67fc98df7181d8b37317dde2f2767fd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "400aa535fc0c28bafb1dd4df45c80217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08db4c91225158f3d884",
+    "url": "/static/css/main.c51ca3ec.chunk.css"
+  },
+  {
+    "revision": "0961ec1b0763b463202b",
+    "url": "/static/js/2.acc46180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acc46180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08db4c91225158f3d884",
+    "url": "/static/js/main.f7cf34e7.chunk.js"
+  },
+  {
+    "revision": "1719815dc35d2184e318",
+    "url": "/static/js/runtime-main.15149bb6.js"
+  },
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/static/media/logo.26ef1cbc.svg"
+  }
+]);
