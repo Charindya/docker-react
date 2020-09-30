d13ddc252a88912073e92120d881dc569b222901
Charindya <charindya@gmail.com>
2020-09-30 18:42:32 +1000

    changed app text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> time to go.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <p className="App-intro">
-        I was changed on the Feature branch.
-      </p>
     </div>
   );
 }
