60de593d38556aca0a2b369f934546d7dfc05497
Charindya <charindya@gmail.com>
2020-09-30 18:37:06 +1000

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,19 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and having to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" /> 
       </header>
+      <p className="App-intro">
+        I was changed on the Feature branch.
+      </p>
     </div>
   );
 }
